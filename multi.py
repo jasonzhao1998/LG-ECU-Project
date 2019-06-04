aad70ac8577b57c098feacaf54f419fd12ca7ae1
Jason Zhao <jasonyz1998@gmail.com>
2019-06-04 15:57:51 -0400

    Update.

--- multi.py
+++ multi.py
@@ -1,5 +1,6 @@
 import os
 import json
+import datetime
 import multiprocessing
 
 '''
@@ -12,6 +13,7 @@ INFO:
 '''
 
 CERT_FILES_DIR = "Z:\\Engineering\\01.OnStar\\11.Flashing\\01.Reflash\\Gen11 Cert Files"
+OUTPUT_DIR = "output/" + "output-" + datetime.datetime.now().strftime("%m-%d-%Y-%H:%M") + '.txt'
 
 
 def read_json(directory):
@@ -20,42 +22,52 @@ def read_json(directory):
 		return d
 
 
-def main():
-	used_dict = read_json("output/used_GB.txt")
-	unused_dict = read_json("output/unused_GB.txt")
-
+def traverse(directory):
+	print(directory)
+	"""
+	output = open("output/output.txt", 'a')
 	total_STID_folders = 0
 	total_amount_ecu_files = 0
 	total_used_ecu_files = 0
+	print("Processing:", f)
+	output.write("\nProcessing:" + f + '\n')
+	for root, dirs, files in os.walk(os.path.join(CERT_FILES_DIR, f)):
+		if os.path.basename(root).isdigit() and len(os.path.basename(root)) == 9:
+			has_ecu = False
+			for file in files:
+				if file[-4:] == '.bin':
+					output.write(file + ' ')
+					has_ecu = True
+					total_amount_ecu_files += 1
+			total_STID_folders += 1
+			if os.path.basename(root) in used_dict:
+				if has_ecu:
+					total_used_ecu_files += 1
+	output.write("\nTotal number of STID folders: " + str(total_STID_folders) + '\n')
+	output.write("Total number of ECU files: " + str(total_amount_ecu_files) + '\n')
+	output.write("Total number of used ECU files: " + str(total_used_ecu_files) + '\n')
+	output.close()
+	total_STID_folders = 0
+	total_amount_ecu_files = 0
+	total_used_ecu_files = 0
+	"""
 
-	output = open("output/output.txt", 'w')
+
+def main():
+	used_dict = read_json("output/used_GB.txt")
+	unused_dict = read_json("output/unused_GB.txt")
+	
+	output = open(OUTPUT_DIR, 'w')
 	output.close()
 	
+	dir_list = []
 	for f in os.listdir(CERT_FILES_DIR):
-		if f[-2:] == 'GB':  # File directory has to end with GB
-			output = open("output/output.txt", 'a')
-			print("Processing:", f)
-			output.write("\nProcessing:" + f + '\n')
-			for root, dirs, files in os.walk(os.path.join(CERT_FILES_DIR, f)):
-				if os.path.basename(root).isdigit() and len(os.path.basename(root)) == 9:
-					has_ecu = False
-					for file in files:
-						if file[-4:] == '.bin':
-							output.write(file + ' ')
-							has_ecu = True
-							total_amount_ecu_files += 1
-					total_STID_folders += 1
-					if os.path.basename(root) in used_dict:
-						if has_ecu:
-							total_used_ecu_files += 1
-			output.write("\nTotal number of STID folders: " + str(total_STID_folders) + '\n')
-			output.write("Total number of ECU files: " + str(total_amount_ecu_files) + '\n')
-			output.write("Total number of used ECU files: " + str(total_used_ecu_files) + '\n')
-			output.close()
-			total_STID_folders = 0
-			total_amount_ecu_files = 0
-			total_used_ecu_files = 0
-		
+		dir_list += [os.path.join(os.path.join(CERT_FILES_DIR, os.path.basename(f)), i) for i in os.listdir(os.path.join(CERT_FILES_DIR, os.path.basename(f)))]
+	
+	while dir_list:
+		p = Process(target=traverse, args=(dir_list.pop(0),))
+		p.start()
+
 
 if __name__ == "__main__":
     main()
