68af605b6681b60f4be5ebf29942dd8ac9a7ed3c
Jason Zhao <jasonyz1998@gmail.com>
2019-06-06 14:02:22 -0400

    Update.

--- multi.py
+++ multi.py
@@ -7,9 +7,7 @@ import multiprocessing
 
 '''
 TODO:
-	Cut 16000 from MY19(20) GB and 1600 from MY20 ERA GB of the unused ECUID files.
-	Maintain a record of STID range what's left and what's cut out.
-	Cut 3800 MX, 3000 NA, 4600 EU, 4600 CN
+	ECUID.bin
 	
 INFO:
 	GEM only has few cert files, so this can be done manually.
@@ -19,10 +17,18 @@ INFO:
 	STID folders on the excel sheets are not moved. 
 '''
 
-CERT_FILES_DIR = r'C:\Users\sarah.pentescu\Desktop\copy cert files'#"Z:\\Engineering\\01.OnStar\\11.Flashing\\01.Reflash\\Gen11 Cert Files"
+# r'C:\Users\sarah.pentescu\Desktop\copy cert files'
+CONSENT = False
+CERT_FILES_DIR = "Z:\\Engineering\\01.OnStar\\11.Flashing\\01.Reflash\\Gen11 Cert Files"
 TOTAL_INFO_OUTPUT_DIR = "output/" + "output-" + datetime.datetime.now().strftime("%m-%d-%Y-%H-%M") + '.txt'
-DIRS_TO_PROCESS = ('MY19(20) GB', 'MY20 TCP ERA GB') # 'MY21 TCP GB', 
+DIRS_TO_PROCESS = ('MY19(20) GB', 'MY20 TCP ERA GB')# 'MY21 TCP GB')
 
+def consent():
+	global CONSENT
+	val = input("Type 'yes' to move ECUIDs: ")
+	if val == 'yes':
+		CONSENT = True
+		print(CONSENT)
 
 def read_json(directory):
 	with open(directory) as file:
@@ -34,15 +40,17 @@ USED_DICT = read_json("output/used_GB.txt")
 UNUSED_DICT = read_json("output/unused_GB.txt")
 
 
-def walk(input):
+def walk(input, consent):
 	directory = input[0]
 	total_STID_folders, total_amount_ecu_files, total_used_ecu_files = 0, 0, 0
+	ecu_counter = 0
 	
 	print("Processing:", directory)
 	
-	sub_info_file = open("output/" + input[1] + '-' + str(os.getpid()) + '.txt', 'w')
-	cut_file = open(os.path.join('output', 'CUT_' + os.path.basename(directory)[:9] + '.txt'), 'w')
-	buffer_file = open(os.path.join('output', 'BUFFER_' + os.path.basename(directory)[:9] + '.txt'), 'w')
+	sub_info_file = open(os.path.join("output", input[1] + '-' + str(os.getpid()) + '.txt'), 'w')
+	cut_file = open(os.path.join('output', 'CUT', 'CUT_' + os.path.basename(directory)[:9] + '.txt'), 'w')
+	buffer_file = open(os.path.join('output', 'BUFFER', 'BUFFER_' + os.path.basename(directory)[:9] + '.txt'), 'w')
+	log_file = open(os.path.join('output', 'LOG_' + os.path.basename(directory)[:9] + '.txt'), 'w')
 	
 	# Determining the amounts of cuts needed
 	total_cut_needed, cur_cut = 0, 0
@@ -59,33 +67,47 @@ def walk(input):
 	
 	for root, dirs, files in os.walk(directory):
 		# If we are in a STID folder
-		if os.path.basename(root).isdigit() and len(os.path.basename(root)) == 9:
+		STID = os.path.basename(root)
+		if STID.isdigit() and len(STID) == 9:
 			total_STID_folders += 1
 			
 			has_ecu = False
 			file_dirs = []
+			ecu_file_dir = ''
 			ecu_dir_name = ''
+			
+			has_txt = False
 			for file in files:
 				if file[-4:] == '.bin':
 					total_amount_ecu_files += 1
 					has_ecu = True
+					ecu_file_dir = os.path.join(root, file)
 					file_dirs.append(os.path.join(root, file))
 					ecu_dir_name = file[:-4]
 				elif file[-4:] == '.txt':
+					has_txt = True
 					file_dirs.append(os.path.join(root, file))
 			
 			if has_ecu:
-				if os.path.basename(root) in USED_DICT or os.path.basename(root) in UNUSED_DICT:
+				if STID in USED_DICT or STID in UNUSED_DICT:
 					total_used_ecu_files += 1
+					log_file.write(STID + ' -> ' + ecu_dir_name + ' ' + 'USED ' + os.path.basename(directory) + ' \n')
 				else:  # Unusd ECUID
 					if cur_cut < total_cut_needed:
+						if os.path.exists('output/ECUID/' + ecu_dir_name):
+							ecu_counter += 1
+							ecu_dir_name += str(ecu_counter)
 						os.mkdir('output/ECUID/' + ecu_dir_name)
-						for dir in file_dirs:
-							shutil.move(dir, "output/ECUID/" + ecu_dir_name)
+						
+						if consent:
+							for dir in file_dirs:
+								print('a')#shutil.move(dir, "output/ECUID/" + ecu_dir_name)
 						cut_file.write(ecu_file_dir + '\n')
 						cur_cut += 1
+						log_file.write(STID + ' -> ' + ecu_dir_name + ' ' + 'CUT ' + os.path.basename(directory) + ' \n')
 					else:
 						buffer_file.write(ecu_file_dir + '\n')
+						log_file.write(STID + ' -> ' + ecu_dir_name + ' ' + 'BUFFER ' + os.path.basename(directory) + ' \n')
 						
 	sub_info_file.write(str(total_STID_folders) + '\n')
 	sub_info_file.write(str(total_amount_ecu_files) + '\n')
@@ -93,12 +115,20 @@ def walk(input):
 	sub_info_file.close()
 	cut_file.close()
 	buffer_file.close()
+	log_file.close()
 	
 	print("Done:", directory)
 	
 
 def integrate():
 	info_output = open(TOTAL_INFO_OUTPUT_DIR, 'w')
+	log_output = open("output/log_file.txt", 'w')
+	
+	for file in os.listdir('output'):
+		if file[:3] == 'LOG':
+			with open('output/' + file, 'r') as subfile:
+				log_output.write("".join(subfile.readlines()))
+			
 	for directory in DIRS_TO_PROCESS:
 		info_output.write(directory + '\n')
 		data = [0] * 3  # 3 is the number of parameters
@@ -112,7 +142,9 @@ def integrate():
 			info_output.write(str(num))
 			info_output.write('\n')
 		info_output.write('\n')
+		
 	info_output.close()
+	log_output.close()
 	
 	
 def main():
@@ -120,9 +152,13 @@ def main():
 	info_output = open(TOTAL_INFO_OUTPUT_DIR, 'w')
 	info_output.close()
 	
-	# Make ECUID directory if does not exist
+	# Make directories if do not exist
 	if not os.path.exists('output/ECUID'):
 		os.mkdir('output/ECUID')
+	if not os.path.exists('output/BUFFER'):
+		os.mkdir('output/BUFFER')
+	if not os.path.exists('output/CUT'):
+		os.mkdir('output/CUT')
 	
 	dir_list = []
 	for directory in os.listdir(CERT_FILES_DIR):
@@ -138,7 +174,7 @@ def main():
 	# Assign tasks to processes
 	process_list = []
 	while dir_list:
-		process = multiprocessing.Process(target=walk, args=(dir_list.pop(0),))
+		process = multiprocessing.Process(target=walk, args=(dir_list.pop(0), CONSENT,))
 		process.start()
 		process_list.append(process)
 	
@@ -147,5 +183,6 @@ def main():
 	
 
 if __name__ == "__main__":
+    consent()
     main()
     integrate()
